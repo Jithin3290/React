2c4290cf61eadc797adc743bf874cdfd3c853521
Jithin C <jithin3290@gmail.com>
2025-06-27 10:46:38 +0530

    Add files via upload

---
+++ t-2/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import { UserCard } from './UserCard';
+function App() {
+  return (
+    <div className="App">
+      <UserCard name = "Raju" age = "30" salary = "1000"></UserCard>
+    </div>
+  );
+}
+
+export default App;
